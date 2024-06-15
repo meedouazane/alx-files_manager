91f9829d108003cb4ed3e352c040ce209d9085a0
Meed <mohamedouazane@yahoo.com>
2024-06-15 19:29:14 +0000

    task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -5,10 +5,7 @@ class AppController {
   static getStatus(req, res) {
     const isDbAlive = dbClient.isAlive();
     const isRedisAlive = redisClient.isAlive();
-
-    if (isDbAlive || isRedisAlive) {
-      res.status(200).send({ redis: isRedisAlive, db: isDbAlive });
-    }
+    res.status(200).send({ redis: isRedisAlive, db: isDbAlive });
   }
 
   static async getStats(req, res) {
