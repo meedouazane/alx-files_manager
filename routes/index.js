f07d19d4b9102474c17caed7ea45a1926fbae566
Meed <mohamedouazane@yahoo.com>
2024-06-15 06:03:38 -0700

    task 2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
+
 const router = express.Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
