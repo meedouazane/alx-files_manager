5c9f1ba3fca69aa383905749fa8d6b46afd812bc
Meed <mohamedouazane@yahoo.com>
2024-06-15 20:23:08 +0000

    controllers//UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,19 +1,18 @@
 import sha1 from 'sha1';
-
 const dbClient = require('../utils/db');
 
 class UsersController {
   static async postNew(res, req) {
     const { email, password } = req.body;
     if (!email) {
-      res.status(400).send('Missing email');
+      res.status(400).send({ error: 'Missing email'});
     }
     if (!password) {
-      res.status(400).send('Missing password');
+      res.status(400).send({error: 'Missing password'});
     }
     const alreadyExist = await dbClient.users.findOne({ email });
     if (alreadyExist) {
-      res.status(400).send('Already exist');
+      res.status(400).send({ error: 'Already exist' });
     }
     const hashed = sha1(password);
     const inserted = await dbClient.collection('users').insertOne({ email, hashed });
