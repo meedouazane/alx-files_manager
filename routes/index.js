9fd959fb5ab26680cfdc59bfd6e8a234af537cff
Meed <mohamedouazane@yahoo.com>
2024-06-19 11:28:29 +0000

    route

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,20 @@ import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.post('/files', FilesController.postUpload);
+const routeIndex = (app) => {
+  app.use('/', router);
+  router.get('/status', (req, res) => {AppController.getStatus(req, res);});
+  router.get('/stats', (req, res) => {AppController.getStats(req, res);});
+  router.post('/users', (req, res) => {UsersController.postNew(req, res);});
+  router.get('/connect', (req, res) => {AuthController.getConnect(req, res);});
 
-module.exports = router;
+  router.get('/disconnect', (req, res) => {AuthController.getDisconnect(req, res);});
+  router.get('/users/me', (req, res) => {UsersController.getMe(req, res);});
+  router.post('/files', (req, res) => { FilesController.postUpload(req, res);});
+  router.get('/files/:id', (req, res) => {FilesController.getShow(req, res);});
+  router.get('/files', (req, res) => {FilesController.getIndex(req, res);});
+  router.put('/files/:id/publish', (req, res) => {FilesController.putPublish(req, res);});
+  router.put('/files/:id/unpublish', (req, res) => {FilesController.putUnpublish(req, res);});
+  router.post('/files/:id/data', (req, res) => {FilesController.getFile(req, res);});
+}
+module.exports = routeIndex;
